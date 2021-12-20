1edad4c5898f24c6feeb05822e7ab92bf895525e
sudiptacc <sudiptacc@users.noreply.github.com>
2021-12-20 00:36:58 -0500

    add layout for resources/meetings section

--- components/Resources.js
+++ components/Resources.js
@@ -1,7 +1,15 @@
+import meetings from '../data/meetings/meetings.json';
+
+/*
+use cards to add the meetings
+*/
+
 export default function Resources() {
+
   return (
-    <div className="container">
-      <h1>WIP</h1>
+    <div className="container my-5">
+    <h1>Resources</h1>
+    add some code here to display meetings from meetings.json
     </div>
   );
 }
