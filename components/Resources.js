452e30a0088e3028ae6ea30f8839c5d621a17e05
sudiptacc <sudiptacc@users.noreply.github.com>
2021-12-21 00:21:22 -0500

    add previous meetings

--- components/Resources.js
+++ components/Resources.js
@@ -1,4 +1,5 @@
-import meetings from '../data/meetings/meetings.json';
+import { unstable_renderSubtreeIntoContainer } from 'react-dom';
+import meetings from '../data/meetings/meetings';
 
 /*
 use cards to add the meetings
@@ -9,7 +10,29 @@ export default function Resources() {
   return (
     <div className="container my-5">
     <h1>Resources</h1>
-    add some code here to display meetings from meetings.json
+    {meetings.map(meeting => {
+      return(
+        <div className="card my-3">
+          <div className="card-body">
+            <h5 className="card-title">
+              <strong>{meeting["number"]}: {meeting["title"]}</strong>
+            </h5>
+            <h6 className="card-subtitle mb-2 text-muted">
+              {meeting["subtitle"]}
+            </h6>
+            <h6 className="card-subtitle mb-2 text-muted">
+              <i>{meeting["date"]}</i>
+            </h6>
+            <p classaName="card-text">
+              {meeting["description"]}
+            </p>
+            <a className="card-link text-primary" href={meeting["link"]} target="_blank" rel="noopener noreferrer">
+              slides!
+            </a>
+          </div>
+        </div>
+        )
+    })}
     </div>
   );
 }
---
+++ data/meetings/meetings.js
@@ -0,0 +1,57 @@
+
+export default [
+    {
+        number: 1,
+        title: "Basic Python",
+        subtitle: "The first steps in Python!",
+        description: "Going over the very basics of Python, as well as an introduction to general programming.",
+        link: "https://docs.google.com/presentation/d/1UtpG-icKOv_c0x5uWjo5XfnNAah19-8No7CCm2CGLDA/edit?usp=sharing",
+        date: "2021-10-28",
+        done: true
+    },
+    {
+        number: 2,
+        title: "Logic and Conditionals",
+        subtitle: "conditionals, logic, booleans",
+        description: "An introduction to conditionals, and how to make choices based on different values.",
+        link: "https://docs.google.com/presentation/d/1--45GsaiQIkWWxz1eBxr-wCB4sAysKydpIUaTOanR_E/edit?usp=sharing",
+        date: "2021-11-04",
+        done: true
+    },
+    {
+        number: 3,
+        title: "Lists and Loops",
+        subtitle: "lists, input, indices, list methods, for loops",
+        description: "Showing lists, and how to add/remove elements to them. Also how to use them and get elements.",
+        link: "https://docs.google.com/presentation/d/17Bfpj2504k2w_X2CkcMrz6xlz9UCtX0d1MM9c678Fo0/edit?usp=sharing",
+        date: "2021-11-18",
+        done: true
+    },
+    {
+        number: 4,
+        title: "Loops and Activity",
+        subtitle: "keywords, while loops, string methods, random, activity!",
+        description: "More about loops, as well as a new type. More methods and a module to make a game.",
+        link: "https://docs.google.com/presentation/d/1oyN2qJ-QFzN2nfl8j0fw_uVJbz2rdtBo15L2BpdC-dU/edit?usp=sharing",
+        date: "2021-12-02",
+        done: true
+    },
+    {
+        number: 5,
+        title: "Functions",
+        subtitle: "functions, parameters and arguments, return",
+        description: "Making functions that can return values, and be used multiple times.",
+        link: "https://docs.google.com/presentation/d/1MEqXklw_YKNcdCNVoo8fL3yidFoODGX1-aDnjfDbAgM/edit?usp=sharing",
+        date: "2021-12-09",
+        done: true
+    },
+    {
+        number: 6,
+        title: "Review",
+        subtitle: "cumulative review! (NO SLIDES)",
+        description: "Just a review of everything done so far, before winter break(!!)",
+        link: "",
+        date: "2021-12-16",
+        done: true
+    }
+]
