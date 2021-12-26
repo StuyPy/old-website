fcb996e19a0d0d614e5f8ae3e40ea295ba732d80
Anthony Chen <50970130+achen318@users.noreply.github.com>
2021-12-26 11:00:23 -0500

    Convert tags to Next.js tags

--- components/Footer.js
+++ components/Footer.js
@@ -12,19 +12,54 @@ export default function Footer() {
     <div className="container-fluid main-bg py-4 text-white text-center">
       <div className="row">
         <div className="container">
-          <a className="p-1" href="https://discord.gg/3kDUYgZGfQ" target="_blank" rel="noopener noreferrer">
-            <Image className="" src={discordLogo} alt="Discord" width="40" height="40" />
+          <a
+            className="p-1"
+            href="https://discord.gg/3kDUYgZGfQ"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className=""
+              src={discordLogo}
+              alt="Discord"
+              width="40"
+              height="40"
+            />
           </a>
-          <a className="p-1" href="https://stuyactivities.org/stuypy" target="_blank" rel="noopener noreferrer">
-          <Image className="" src={stuyActLogo} alt="Stuy Activites" width="40" height="40" />
+          <a
+            className="p-1"
+            href="https://stuyactivities.org/stuypy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className=""
+              src={stuyActLogo}
+              alt="Stuy Activites"
+              width="40"
+              height="40"
+            />
           </a>
-          <a className="p-1" href="https://www.instagram.com/stuypy/" target="_blank" rel="noopener noreferrer">
-          <Image className="" src={instagramLogo} alt="Instagram" width="40" height="40" />
+          <a
+            className="p-1"
+            href="https://www.instagram.com/stuypy/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className=""
+              src={instagramLogo}
+              alt="Instagram"
+              width="40"
+              height="40"
+            />
           </a>
         </div>
       </div>
       <div className="container">
-        <a className="text-center">&copy; {date.getFullYear()} StuyPy. All rights reserved.</a>
+        <a className="text-center">
+          &copy; {date.getFullYear()} StuyPy. All rights reserved.
+        </a>
       </div>
     </div>
   );
